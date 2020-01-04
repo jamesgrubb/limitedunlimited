b33095f627da7abfbc2c29736efc1bfdf3fce534
jamesgrubb <jamesgrubb@mac.com>
2020-01-04 23:02:04 +0000

    new changes

--- functions/chrome/chrome.js
+++ functions/chrome/chrome.js
@@ -2,8 +2,8 @@ const chromium = require('chrome-aws-lambda')
 const puppeteer = require('puppeteer-core')
 
 exports.handler = async (event, context, callback) => {
-  let theTitle = null
-  let browser = null
+  // let theTitle = null
+  // let browser = null
   console.log('spawning chrome headless')
   try {
     const executablePath = await chromium.executablePath
@@ -20,15 +20,28 @@ exports.handler = async (event, context, callback) => {
     const targetUrl = 'https://davidwells.io'
 
     // Goto page and then do stuff
+    
+    const page = await browser.newPage();
     await page.goto(targetUrl, {
       waitUntil: ["domcontentloaded", "networkidle0"]
-    })
+    });
+    
+    console.log(await page.content());
+    const pdf = await page.pdf();
+    
+    await browser.close();
+    console.log(pdf) // maybe do response(pdf).type('x-pdf')
 
-    await page.waitForSelector('#phenomic')
+    //  Paste code
+    // await page.goto(targetUrl, {
+    //   waitUntil: ["domcontentloaded", "networkidle0"]
+    // })
 
-    theTitle = await page.title();
+    // await page.waitForSelector('#phenomic')
 
-    console.log('done on page', theTitle)
+    // theTitle = await page.title();
+
+    // console.log('done on page', theTitle)
 
   } catch (error) {
     console.log('error', error)
@@ -45,10 +58,10 @@ exports.handler = async (event, context, callback) => {
     }
   }
 
-  return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      title: theTitle,
-    })
-  })
+  // return callback(null, {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     title: theTitle,
+  //   })
+  // })
 }
--- src/App.js
+++ src/App.js
@@ -2,20 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
+  
+
+  const makePdfHandler = (e) => {
+    e.preventDefault()
+
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      
+  };
+
+  
+
+  fetch("/.netlify/functions/chrome", options)
+  
+      .catch((err) => {
+          console.log(err)
+  
+      });
+
+  }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <button onClick={makePdfHandler}>PDF</button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
-  }
+  
 }
 
 export default App;
